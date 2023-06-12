c2c3ce263a0489435922348cc7f012ce11cf7f90
Rick <13621099447@163.com>
2023-06-12 15:31:25 +0800

    feat: add import pk and validate encrypt data

--- app/utils/index.js
+++ app/utils/index.js
@@ -1,5 +1,28 @@
+import database from '../Database';
+import Encrypt from './encrypt';
+
+const encrypt = new Encrypt();
+
 export const camelCase = str => {
   return str.replace(/_([a-z])/g, function (match, group1) {
     return group1.toUpperCase();
   });
+};
+
+export const validateDuplicateVault = async (password, data) => {
+  const records = await database.get('vault').query().fetch();
+  let ret = false;
+  if (records.length) {
+    for (let i = 0; i < records.length; i++) {
+      let {data: decryptData} = await encrypt.decrypt(
+        password,
+        records[i].data,
+      );
+      if (decryptData === data) {
+        ret = true;
+        break;
+      }
+    }
+  }
+  return ret;
 };
--- app/utils/validateDuplicateRecords.js
+++
@@ -1,7 +0,0 @@
-import {Q} from '@nozbe/watermelondb';
-import database from '../Database';
-const getDuplicatesByContent = async (tableName, contentName, content) => {
-  return database.get(tableName).query(Q.where(contentName, Q.eq(content)));
-};
-
-export {getDuplicatesByContent};
