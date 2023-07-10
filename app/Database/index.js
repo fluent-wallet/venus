d3154579e959af2346f78087de483dcea68d4bfa
Rick <13621099447@163.com>
2023-07-10 11:41:47 +0800

    fix: fix case dir

---
+++ app/Database/index.js
@@ -0,0 +1,26 @@
+// import {Platform} from 'react-native';
+import {Database} from '@nozbe/watermelondb';
+import SQLiteAdapter from '@nozbe/watermelondb/adapters/sqlite';
+
+import schema from './schema';
+import migrations from './migrations';
+import * as models from './model';
+
+const adapter = new SQLiteAdapter({
+  dbName: 'venus_database2',
+  schema,
+  migrations,
+  // RN not support synchronous mode yet(ps: jsi === true means enable synchronous mode).see below:
+  // https://github.com/facebook/react-native/issues/26705
+  // https://github.com/Nozbe/WatermelonDB/issues/813
+  // This bug may only appear in debug mode.We will continue to watch...
+  jsi: false, // jsi: Platform.OS === 'ios',
+  onSetUpError: error => {},
+});
+
+const database = new Database({
+  adapter,
+  modelClasses: [...Object.values(models)],
+});
+
+export default database;
