a3fc6fc61c05410976fe99aa3ec3f7eaabb14868
iosh <454053152@qq.com>
2023-09-28 15:26:44 +0800

    feat: add nativewind package

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./packages/ui/App.{js,jsx,ts,tsx}", "./packages/ui/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
