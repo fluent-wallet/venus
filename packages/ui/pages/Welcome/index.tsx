683ab8e561925f1bf2a6f88a4be1c8fc4501ac2c
SSSensational <37439786+SSSensational@users.noreply.github.com>
2023-10-07 17:22:23 +0800

    merge:delete old db code

---
+++ packages/ui/pages/Welcome/index.tsx
@@ -0,0 +1,15 @@
+import type { PropsWithChildren } from 'react';
+import { ScrollView, StatusBar, useColorScheme, View, Text } from 'react-native';
+import { DebugInstructions, Header, LearnMoreLinks, ReloadInstructions } from 'react-native/Libraries/NewAppScreen';
+
+function Welcome() {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={{ flex: 1 }}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <Text> Welcome </Text>
+    </View>
+  );
+}
+
+export default Welcome;
