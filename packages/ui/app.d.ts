de5f3ee52020548c78f15e62ce8eb3ae2f641c61
iosh <454053152@qq.com>
2023-10-09 16:51:52 +0800

    chore: remove tailwind theme add rneui theme

--- packages/ui/app.d.ts
+++ packages/ui/app.d.ts
@@ -1,8 +1,25 @@
 /// <reference types="nativewind/types" />
+import '@rneui/themed';
 
-declare module "*.svg" {
-  import React from "react";
-  import { SvgProps } from "react-native-svg";
+declare module '*.svg' {
+  import React from 'react';
+  import { SvgProps } from 'react-native-svg';
   const content: React.FC<SvgProps>;
   export default content;
+}
+
+declare module '@rneui/themed' {
+  export interface Colors {
+    textColorPrimary: string;
+    textColorSecondary: string;
+    textColorBrand: string;
+    textColorInvert: string;
+    borderColorPrimary: string;
+    surfacePrimary: string;
+    surfaceSecondary: string;
+    surfaceBrand: string;
+    surfaceInvert: string;
+    warnSuccessPrimary: string;
+    warnErrorPrimary: string;
+  }
 }
