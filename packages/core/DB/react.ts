2a44a7d8b8b408b0bf6231edce9a0a3d74055470
SSSensational <37439786+SSSensational@users.noreply.github.com>
2023-10-24 14:51:45 +0800

    fix: db-compose

--- packages/core/DB/react.ts
+++ packages/core/DB/react.ts
@@ -1,14 +1,14 @@
-import { withDatabase, withObservables as _withObservables } from '@nozbe/watermelondb/react';
+import { withDatabase, withObservables as _withObservables, compose as _compose } from '@nozbe/watermelondb/react';
 export { withDatabase, withObservables } from '@nozbe/watermelondb/react';
-import { flow } from 'lodash-es';
+import { type flow } from 'lodash-es';
 import _database from '.';
 import TableName from './TableName';
-export { flow as compose } from 'lodash-es';
 
+export const compose = _compose as typeof flow;
 export type Database = typeof _database;
 
 export function withObservablesFromDB(observeModels: Array<TableName>) {
-  return flow(
+  return compose(
     withDatabase,
     _withObservables([], ({ database }: { database: Database }) =>
       Object.fromEntries(observeModels.map((model) => [model, database.collections.get(model).query().observe()]))
