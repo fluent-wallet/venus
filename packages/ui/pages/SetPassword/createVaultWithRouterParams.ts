d3b1b1c3b834b7bca5d83ecdd1872664e4e88f21
iosh <454053152@qq.com>
2023-10-30 10:46:02 +0800

    feat: add create wallet by BSIM

--- packages/ui/pages/SetPassword/createVaultWithRouterParams.ts
+++ packages/ui/pages/SetPassword/createVaultWithRouterParams.ts
@@ -1,5 +1,8 @@
-import { createPrivateKeyVault, createHDVault } from "@core/DB/models/Vault/service";
-import { type RootStackList } from "@router/configs";
+import BSIMSDK, { CoinTypes } from '@core/BSIMSDK';
+import { createPrivateKeyVault, createHDVault, createBSIMVault } from '@core/DB/models/Vault/service';
+import { addHexPrefix } from '@core/utils/base';
+import { type RootStackList } from '@router/configs';
+import { computeAddress } from 'ethers';
 
 const createVaultWithRouterParams = async (args?: RootStackList['Biometrics']) => {
   if (args?.type === 'importPrivateKey' && args.value) {
@@ -8,6 +11,14 @@ const createVaultWithRouterParams = async (args?: RootStackList['Biometrics']) =
   if (args?.type === 'importSeedPhrase' && args.type) {
     return await createHDVault(args.value);
   }
+  if (args?.type === 'BSIM') {
+    BSIMSDK.create('BSIM');
+    const BSIMKey = await BSIMSDK.genNewKey(CoinTypes.CONFLUX);
+    const pubkey = addHexPrefix(BSIMKey.key.substring(46, 176));
+    const hexAddress = computeAddress(pubkey);
+    return await createBSIMVault({ hexAddress: hexAddress, index: `${BSIMKey.index}` });
+  }
+
   return await createHDVault();
 };
 export default createVaultWithRouterParams;
