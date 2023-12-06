d8f7bdef30cc415df22b1c3d1b79dabf76fce309
mosi <mosshengqi@outlook.com>
2023-12-06 15:10:52 +0800

    feat: update tx/txPayload/txExtra db

--- packages/core/database/models/TxExtra/index.ts
+++ packages/core/database/models/TxExtra/index.ts
@@ -6,17 +6,17 @@ import TableName from '../../TableName';
 export class TxExtra extends Model {
   static table = TableName.TxExtra;
   static associations = {
-    [TableName.Tx]: { type: 'has_many', foreignKey: 'asset_id' },
+    [TableName.Tx]: { type: 'has_many', foreignKey: 'tx_extra_id' },
   } as const;
 
-  @field('ok') ok!: boolean | null;
-  @field('contract_creation') contractCreation!: boolean | null;
-  @field('simple') simple!: boolean;
-  @text('send_action') sendAction!: string;
-  @field('contract_interaction') contractInteraction!: boolean | null;
-  @field('token20') token20!: boolean | null;
-  @field('token_nft') tokenNft!: boolean;
-  @text('address') address!: string;
-  @text('method') method!: string | null;
+  @field('ok') ok!: boolean | null; // extra data is finished
+  @field('contract_creation') contractCreation!: boolean | null; // contract creation tx
+  @field('simple') simple!: boolean; // simple tx
+  @text('send_action') sendAction!: string; // speedup or cancel
+  @field('contract_interaction') contractInteraction!: boolean | null; // contract interaction tx
+  @field('token20') token20!: boolean | null; // 20 contract
+  @field('token_nft') tokenNft!: boolean; // nft contract
+  @text('address') address!: string; // intresting address of this tx, usually recipient
+  @text('method') method!: string | null; // contract call method name
   @children(TableName.Tx) txs!: Query<Tx>;
 }
