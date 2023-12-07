eac9f472aa874ce4495ed75500b3293b4279e20f
iosh <454053152@qq.com>
2023-12-07 14:48:03 +0800

    chore: add toast message to copy address

--- packages/ui/pages/Wallet/WalletHeader.tsx
+++ packages/ui/pages/Wallet/WalletHeader.tsx
@@ -4,21 +4,41 @@ import SwitchCurrentNetwork from '@components/SwitchCurrentNetwork';
 import { useCurrentAddressValue } from '@core/WalletCore/Plugins/ReactInject';
 import { useNavigation } from '@react-navigation/native';
 import { AccountSelectStackName, ReceiveAddressStackName, ScanQRCodeStackName, StackNavigation } from '@router/configs';
-import { Text } from '@rneui/themed';
+import { Icon, Text, useTheme } from '@rneui/themed';
 import { shortenAddress } from '@core/utils/address';
 import CopyAll from '@assets/icons/copy_all.svg';
 import Flip from '@assets/icons/flip.svg';
 import Menu from '@assets/icons/menu.svg';
+import { statusBarHeight } from '@utils/deviceInfo';
+import { showMessage, hideMessage } from 'react-native-flash-message';
 
 const HeaderTitle: React.FC<{ backgroundColor: string }> = ({ backgroundColor }: { backgroundColor: string }) => {
+  const { theme } = useTheme();
   const currentAddressValue = useCurrentAddressValue();
   if (!currentAddressValue) return null;
   return (
-    <TouchableHighlight onPress={() => Clipboard.setString(currentAddressValue)} className="rounded-full overflow-hidden">
-      <View className="bg-white flex flex-row px-[12px] py-[8px] rounded-full" style={{ backgroundColor }}>
+    <TouchableHighlight
+      onPress={() => {
+        Clipboard.setString(currentAddressValue);
+        showMessage({
+          type: 'success',
+          floating: true,
+          statusBarHeight: statusBarHeight + 48,
+          message: 'Copied',
+          icon: () => <Icon name="done" color={theme.colors.textInvert} />,
+          backgroundColor: theme.colors.warnSuccessPrimary,
+          color: theme.colors.textInvert,
+          textStyle: { fontSize: 12, fontFamily: 'SF Pro Display' },
+          style: { display: 'flex', alignItems: 'center', alignSelf: 'center', width: 110, },
+          duration: 1.5 * 1000, // 1.5s
+        });
+      }}
+      className="rounded-full overflow-hidden"
+    >
+      <View className="bg-white flex flex-row px-3 py-2 rounded-full items-center" style={{ backgroundColor }}>
         <Text className="text-[10px]">{shortenAddress(currentAddressValue)}</Text>
-        <View className="pl-[4px]">
-          <CopyAll />
+        <View className="ml-1 p-[3px] rounded-full" style={{ backgroundColor: theme.colors.surfaceBrand }}>
+          <CopyAll color={theme.colors.surfaceSecondary} />
         </View>
       </View>
     </TouchableHighlight>
