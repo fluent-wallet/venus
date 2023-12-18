9daf0f0a194e94c0cd08357224568c6c0d865f5e
SSSensational <37439786+SSSensational@users.noreply.github.com>
2023-12-18 18:58:21 +0800

    tmp nft stickyHeader

--- packages/ui/modules/AssetList/ESpaceNFTList/NFTItem.tsx
+++ packages/ui/modules/AssetList/ESpaceNFTList/NFTItem.tsx
@@ -9,8 +9,8 @@ import { type Network } from '@core/database/models/Network';
 import { type Address } from '@core/database/models/Address';
 import { AssetType } from '@core/database/models/Asset';
 import useInAsync from '@hooks/useInAsync';
-import MixinImage from '@components/MixinImage';
 import Skeleton from '@components/Skeleton';
+import MixinImage from '@components/MixinImage';
 import TokenIconDefault from '@assets/icons/defaultToken.svg';
 import DefaultNFTImage from '@assets/images/NFT.svg';
 import { type NFTItemDetail, type NFTWithDetail } from '.';
@@ -39,6 +39,7 @@ const NFTItem: React.FC<{
   currentAddress: Address;
 }> = ({ onPress, onSelectNftItem, data, isExpanded, currentNetwork, currentAddress }) => {
   const { theme } = useTheme();
+
   const [details, setDetails] = useState<Array<NFTItemDetail> | null>(null);
 
   const fetchCurrentNFTDetail = useCallback(async () => {
@@ -63,81 +64,80 @@ const NFTItem: React.FC<{
   }, [isExpanded]);
 
   return (
-    <View className="flex flex-1 w-full px-2">
-      <ListItem.Accordion
-        isExpanded={isExpanded}
-        onPress={onPress}
-        containerStyle={{
-          backgroundColor: theme.colors.normalBackground,
-          display: 'flex',
-          flex: 1,
-          justifyContent: 'space-between',
-        }}
-        icon={<Icon name="keyboard-arrow-down" color={theme.colors.contrastWhiteAndBlack} />}
-        content={
-          <View className="flex flex-row items-center">
-            {data.icon ? (
-              <MixinImage source={{ uri: data.icon }} width={32} height={32} fallback={<TokenIconDefault width={32} height={32} />} />
-            ) : (
-              <TokenIconDefault width={32} height={32} />
-            )}
-            <Text style={{ color: theme.colors.contrastWhiteAndBlack }} className="text-base font-medium leading-6 ml-2">
-              {data.name}
-            </Text>
-          </View>
-        }
-      >
-        {isExpanded && (
-          <ListItem containerStyle={{ backgroundColor: 'transparent' }}>
-            <View className="flex flex-row flex-wrap justify-between w-full">
-              {inFetch && !details && (
-                <View className="flex-1 flex flex-row justify-between">
-                  {Array.from({ length: 2 }).map((_, i) => (
-                    <View key={i} style={{ backgroundColor: theme.colors.surfaceCard }} className="p-3 mb-3 rounded-md">
-                      <View className="w-36 h-36 mb-2">
-                        <Skeleton width={142} height={142} />
-                      </View>
-                      <View className="mb-1">
-                        <Skeleton width={70} height={16} />
-                      </View>
-                      <View>
-                        <Skeleton width={141} height={16} />
-                      </View>
+    <ListItem.Accordion
+      isExpanded={isExpanded}
+      onPress={onPress}
+      containerStyle={{
+        backgroundColor: theme.colors.normalBackground,
+        display: 'flex',
+        flex: 1,
+        justifyContent: 'space-between',
+      }}
+      style={{ paddingHorizontal: 8 }}
+      icon={<Icon name="keyboard-arrow-down" color={theme.colors.contrastWhiteAndBlack} />}
+      content={
+        <View className="flex flex-row items-center">
+          {data.icon ? (
+            <MixinImage source={{ uri: data.icon }} width={32} height={32} fallback={<TokenIconDefault width={32} height={32} />} />
+          ) : (
+            <TokenIconDefault width={32} height={32} />
+          )}
+          <Text style={{ color: theme.colors.contrastWhiteAndBlack }} className="text-base font-medium leading-6 ml-2">
+            {data.name}
+          </Text>
+        </View>
+      }
+    >
+      {isExpanded && (
+        <ListItem containerStyle={{ backgroundColor: 'transparent' }}>
+          <View className="flex flex-row flex-wrap justify-between w-full">
+            {inFetch && !details && (
+              <View className="flex-1 flex flex-row justify-between">
+                {Array.from({ length: 2 }).map((_, i) => (
+                  <View key={i} style={{ backgroundColor: theme.colors.surfaceCard }} className="p-3 mb-3 rounded-md">
+                    <View className="w-36 h-36 mb-2">
+                      <Skeleton width={142} height={142} />
                     </View>
-                  ))}
-                </View>
-              )}
-
-              {details &&
-                details.map((detail) => (
-                  <Pressable key={detail.tokenId} style={{ width: '48%' }} onPress={() => onSelectNftItem?.({ ...data, detail })}>
-                    <View style={{ backgroundColor: theme.colors.surfaceCard }} className="p-3 mb-3 rounded-md w-full">
-                      {detail.amount && data.type === AssetType.ERC1155 && (
-                        <View className="absolute top-4 right-4 z-10 px-2 rounded-full" style={{ backgroundColor: theme.colors.surfaceCard }}>
-                          <Text style={{ color: theme.colors.textPrimary }}>x{detail.amount}</Text>
-                        </View>
-                      )}
-                      <View className="flex items-center w-full h-36 overflow-hidden">
-                        {detail.icon ? (
-                          <MixinImage source={{ uri: detail.icon }} fallback={<DefaultNFTImage />} className="w-full h-full" />
-                        ) : (
-                          <DefaultNFTImage />
-                        )}
-                      </View>
-                      <Text style={{ color: theme.colors.textSecondary }} className="text-sm leading-6">
-                        {data.name}
-                      </Text>
-                      <Text style={{ color: theme.colors.contrastWhiteAndBlack }} className="text-sm leading-6">
-                        {detail.name} #{detail.tokenId}
-                      </Text>
+                    <View className="mb-1">
+                      <Skeleton width={70} height={16} />
                     </View>
-                  </Pressable>
+                    <View>
+                      <Skeleton width={141} height={16} />
+                    </View>
+                  </View>
                 ))}
-            </View>
-          </ListItem>
-        )}
-      </ListItem.Accordion>
-    </View>
+              </View>
+            )}
+
+            {details &&
+              details.map((detail) => (
+                <Pressable key={detail.tokenId} style={{ width: '48%' }} onPress={() => onSelectNftItem?.({ ...data, detail })}>
+                  <View style={{ backgroundColor: theme.colors.surfaceCard }} className="p-3 mb-3 rounded-md w-full">
+                    {detail.amount && data.type === AssetType.ERC1155 && (
+                      <View className="absolute top-4 right-4 z-10 px-2 rounded-full" style={{ backgroundColor: theme.colors.surfaceCard }}>
+                        <Text style={{ color: theme.colors.textPrimary }}>x{detail.amount}</Text>
+                      </View>
+                    )}
+                    <View className="flex items-center w-full h-36 overflow-hidden">
+                      {detail.icon ? (
+                        <MixinImage source={{ uri: detail.icon }} fallback={<DefaultNFTImage />} className="w-full h-full" />
+                      ) : (
+                        <DefaultNFTImage />
+                      )}
+                    </View>
+                    <Text style={{ color: theme.colors.textSecondary }} className="text-sm leading-6">
+                      {data.name}
+                    </Text>
+                    <Text style={{ color: theme.colors.contrastWhiteAndBlack }} className="text-sm leading-6">
+                      {detail.name} #{detail.tokenId}
+                    </Text>
+                  </View>
+                </Pressable>
+              ))}
+          </View>
+        </ListItem>
+      )}
+    </ListItem.Accordion>
   );
 };
 
--- packages/ui/modules/AssetList/ESpaceNFTList/index.tsx
+++ packages/ui/modules/AssetList/ESpaceNFTList/index.tsx
@@ -1,8 +1,12 @@
-import { useEffect, useState } from 'react';
-import { ScrollView, View } from 'react-native';
+import { useEffect, useState, useMemo, useCallback } from 'react';
+import { ScrollView, View, type NativeSyntheticEvent, type NativeScrollEvent } from 'react-native';
+import { Icon } from '@rneui/base';
+import { useTheme, Text, ListItem } from '@rneui/themed';
 import { useCurrentAddress, useCurrentNetwork, useAssetsNFTList } from '@core/WalletCore/Plugins/ReactInject';
 import { type AssetInfo } from '@core/WalletCore/Plugins/AssetsTracker/types';
 import Skeleton from '@components/Skeleton';
+import MixinImage from '@components/MixinImage';
+import TokenIconDefault from '@assets/icons/defaultToken.svg';
 import NFTItem from './NFTItem';
 
 export interface NFTItemDetail {
@@ -16,46 +20,99 @@ export interface NFTItemDetail {
 export type NFTWithDetail = AssetInfo & { detail: NFTItemDetail };
 
 const ESpaceNFTList: React.FC<{ onSelectNftItem?: (nft: NFTWithDetail) => void }> = ({ onSelectNftItem }) => {
+  const { theme } = useTheme();
+
   const nfts = useAssetsNFTList();
   const currentAddress = useCurrentAddress()!;
   const currentNetwork = useCurrentNetwork()!;
-  const [currentOpenNft, setCurrentOpenNft] = useState<string | null>(null);
+  const [currentOpenNftContract, _setCurrentOpenNftContract] = useState<string | null>(null);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const currentOpenNft = useMemo(() => nfts?.find((nft) => nft.contractAddress === currentOpenNftContract), [currentOpenNftContract]);
+  const currentOpenNftPosition = useMemo(() => {
+    const index = nfts?.findIndex((nft) => nft.contractAddress === currentOpenNftContract);
+    if (typeof index !== 'number') return null;
+    return (index + 1) * 48;
+  }, [currentOpenNftContract]);
+
+  const [isCurrentOpenHeaderInView, setCurrentOpenHeaderInView] = useState(true);
+  const setCurrentOpenNftContract = useCallback((nft: string | null) => {
+    _setCurrentOpenNftContract(nft);
+    if (nft === null) {
+      setCurrentOpenHeaderInView(true);
+    }
+  }, []);
 
   useEffect(() => {
-    setCurrentOpenNft(null);
+    setCurrentOpenNftContract(null);
   }, [currentNetwork?.chainId, currentAddress?.hex]);
+
+
+  const handleScroll = useCallback((evt: NativeSyntheticEvent<NativeScrollEvent>) => {
+    if (currentOpenNftPosition === null) return;
+    if (evt.nativeEvent.contentOffset.y > (currentOpenNftPosition)) {
+      setCurrentOpenHeaderInView(false);
+    } else {
+      setCurrentOpenHeaderInView(true);
+    }
+  }, [currentOpenNftPosition]);
 
   if (!currentAddress || !currentNetwork) {
     return null;
   }
 
   return (
-    <ScrollView>
-      {nfts === null ? (
-        <View className="flex-1 ">
-          <View className="flex flex-row items-center p-6">
-            <Skeleton circle width={32} height={32} style={{ marginRight: 8 }} />
-            <Skeleton width={70} height={16} />
-          </View>
-          <View className="flex flex-row items-center p-6">
-            <Skeleton circle width={32} height={32} style={{ marginRight: 8 }} />
-            <Skeleton width={70} height={16} />
-          </View>
-        </View>
-      ) : (
-        nfts.map((nft) => (
-          <NFTItem
-            data={nft}
-            key={nft.contractAddress}
-            isExpanded={currentOpenNft === nft.contractAddress}
-            onPress={() => setCurrentOpenNft((pre) => (pre === nft.contractAddress! ? null : nft.contractAddress!))}
-            onSelectNftItem={onSelectNftItem}
-            currentNetwork={currentNetwork}
-            currentAddress={currentAddress}
-          />
-        ))
+    <View className="flex-1">
+      {currentOpenNft && !isCurrentOpenHeaderInView && (
+        <ListItem.Accordion
+          onPress={() => setCurrentOpenNftContract(null)}
+          containerStyle={{
+            backgroundColor: theme.colors.normalBackground,
+            display: 'flex',
+            justifyContent: 'space-between',
+          }}
+          style={{ paddingHorizontal: 8 }}
+          icon={<Icon name="keyboard-arrow-down" color={theme.colors.contrastWhiteAndBlack} />}
+          content={
+            <View className="flex flex-row items-center">
+              {currentOpenNft.icon ? (
+                <MixinImage source={{ uri: currentOpenNft.icon }} width={32} height={32} fallback={<TokenIconDefault width={32} height={32} />} />
+              ) : (
+                <TokenIconDefault width={32} height={32} />
+              )}
+              <Text style={{ color: theme.colors.contrastWhiteAndBlack }} className="text-base font-medium leading-6 ml-2">
+                {currentOpenNft.name}
+              </Text>
+            </View>
+          }
+        />
       )}
-    </ScrollView>
+      <ScrollView className="flex-1" onScroll={handleScroll}>
+        {nfts === null ? (
+          <View className="flex-1 ">
+            <View className="flex flex-row items-center p-6">
+              <Skeleton circle width={32} height={32} style={{ marginRight: 8 }} />
+              <Skeleton width={70} height={16} />
+            </View>
+            <View className="flex flex-row items-center p-6">
+              <Skeleton circle width={32} height={32} style={{ marginRight: 8 }} />
+              <Skeleton width={70} height={16} />
+            </View>
+          </View>
+        ) : (
+          nfts.map((nft) => (
+            <NFTItem
+              data={nft}
+              key={nft.contractAddress}
+              isExpanded={currentOpenNftContract === nft.contractAddress}
+              onPress={() => setCurrentOpenNftContract((pre) => (pre === nft.contractAddress! ? null : nft.contractAddress!))}
+              onSelectNftItem={onSelectNftItem}
+              currentNetwork={currentNetwork}
+              currentAddress={currentAddress}
+            />
+          ))
+        )}
+      </ScrollView>
+    </View>
   );
 };
 
