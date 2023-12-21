727ccb1e87cea1c7dcbce8f0baca452ced645fd8
mosi <mosshengqi@outlook.com>
2023-12-21 10:41:40 +0800

    feat: add ReceiveFunds

--- packages/ui/modules/AssetList/TokenList/index.tsx
+++ packages/ui/modules/AssetList/TokenList/index.tsx
@@ -4,19 +4,31 @@ import { useAssetsTokenList } from '@core/WalletCore/Plugins/ReactInject';
 import { type AssetInfo } from '@core/WalletCore/Plugins/AssetsTracker/types';
 import TokenItem from './TokenItem';
 import SkeletonList from './SkeletonList';
+import ReceiveFunds from './ReceiveFunds';
 
-const TokenList: React.FC<{ onPress?: (v: AssetInfo) => void; skeleton?: number; RenderList?: typeof FlatList | typeof BottomSheetFlatList }> = ({
-  onPress,
-  skeleton = 8,
-  RenderList = FlatList,
-}) => {
+const TokenList: React.FC<{
+  onPress?: (v: AssetInfo) => void;
+  skeleton?: number;
+  RenderList?: typeof FlatList | typeof BottomSheetFlatList;
+  enableEmpty?: boolean;
+}> = ({ onPress, skeleton = 8, RenderList = FlatList, enableEmpty = false }) => {
   const tokens = useAssetsTokenList();
-
-  return tokens === null ? (
-    <View className="flex-1 px-6 py-4">
-      <SkeletonList length={skeleton} />
-    </View>
-  ) : (
+  const empty = !tokens || tokens.every((v) => BigInt(v.balance) <= 0);
+  if (tokens === null) {
+    return (
+      <View className="flex-1 px-6 py-4">
+        <SkeletonList length={skeleton} />
+      </View>
+    );
+  }
+  if (empty && enableEmpty) {
+    return (
+      <View className="flex-1 px-6 py-4">
+        <ReceiveFunds />
+      </View>
+    );
+  }
+  return (
     <RenderList className="flex flex-1 px-6 py-4" data={tokens} renderItem={({ item }) => <TokenItem data={item} onPress={onPress ? onPress : undefined} />} />
   );
 };
