e6634f536f81325d8e974eea66fc8db7a0a33953
mosi <mosshengqi@outlook.com>
2023-12-28 18:32:41 +0800

    feat: delete tx tableSchema

--- packages/core/database/migrations/index.ts
+++ packages/core/database/migrations/index.ts
@@ -3,19 +3,19 @@ import TableName from '../TableName';
 
 const migrations = schemaMigrations({
   migrations: [
-    {
-      // ⚠️ Set this to a number one larger than the current schema version
-      toVersion: 2,
-      steps: [
-        // See "Migrations API" for more details
-        addColumns({
-          table: TableName.Tx,
-          columns: [
-            { name: 'executed_at', type: 'number', isOptional: true },
-          ],
-        }),
-      ],
-    },
+    // {
+    //   // ⚠️ Set this to a number one larger than the current schema version
+    //   toVersion: 2,
+    //   steps: [
+    //     // See "Migrations API" for more details
+    //     addColumns({
+    //       table: TableName.Tx,
+    //       columns: [
+    //         { name: 'executed_at', type: 'number', isOptional: true },
+    //       ],
+    //     }),
+    //   ],
+    // },
   ],
 });
 
