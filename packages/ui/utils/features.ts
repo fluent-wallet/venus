a906ff25923e7911999f8a5d27c8cc2bf8780121
iosh <454053152@qq.com>
2024-01-10 17:13:23 +0800

    chore: update feature config

---
+++ packages/ui/utils/features.ts
@@ -0,0 +1,32 @@
+import { getAppEnv, isDev, isQA } from './getEnv';
+
+export const RESET_WALLET_DATA_FEATURE = {
+  describe: 'Reset wallet data button',
+  allow: isDev,
+};
+
+export const APP_VERSION_FLAG_FEATURE = {
+  describe: 'add dev or qa flag to app version',
+  allow: isQA || isDev,
+  value: getAppEnv(),
+};
+
+export const SWITCH_NETWORK_FEATURE = {
+  describe: 'switch network button',
+  allow: isQA || isDev,
+};
+
+export const ADD_ACCOUNT_FEATURE = {
+  describe: 'add account button',
+  allow: isQA || isDev,
+};
+
+export const WELCOME_CREATE_WALLET_FEATURE = {
+  describe: 'welcome create wallet button',
+  allow: isQA || isDev,
+};
+
+export const WELCOME_IMPORT_WALLET_FEATURE = {
+  describe: 'welcome import wallet button',
+  allow: isQA || isDev,
+};
