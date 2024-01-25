c2456c7d584e0f03aed9cc8cf2ce2771a70d0f7d
iosh <454053152@qq.com>
2024-01-25 09:50:50 +0800

    chore: add loading icon

--- packages/ui-new/components/Button/Hourglass/index.tsx
+++ packages/ui-new/components/Button/Hourglass/index.tsx
@@ -1,16 +1,13 @@
 import React, { memo } from 'react';
-import { type StyleProp, type ViewStyle } from 'react-native';
+import { type StyleProp, type ViewStyle, View } from 'react-native';
 import Svg, { Path } from 'react-native-svg';
-import Animated, { useSharedValue, useAnimatedProps, withTiming, withRepeat, interpolate } from 'react-native-reanimated';
+import LottieView from 'lottie-react-native';
 
 const Hourglass: React.FC<{ color: string; style: StyleProp<ViewStyle> }> = memo(({ color, style }) => {
   return (
-    <Svg width="12" height="18" viewBox="0 0 12 18" fill="none" style={style}>
-      <Path
-        d="M11.4 18v-5.4h-.01l.01-.009L7.8 9l3.6-3.6-.01-.009h.01V0H.6v5.391h.009l-.01.009L4.2 9 .6 12.591l.01.009h-.01V18h10.8zm-9-13.05V1.8h7.2v3.15L6 8.55l-3.6-3.6zM6 9.45l3.6 3.6v3.15H2.4v-3.15L6 9.45z"
-        fill={color}
-      />
-    </Svg>
+    <View style={style}>
+      <LottieView source={require('./hourglass.json')} style={{ width: '100%', height: '100%' }} autoPlay loop />
+    </View>
   );
 });
 
