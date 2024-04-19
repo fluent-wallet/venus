ca39d606d44bfc18510b9ab31fe94666bb315118
iosh <454053152@qq.com>
2024-04-19 11:53:54 +0800

    chore: add flag to change network

--- packages/ui-new/utils/features.ts
+++ packages/ui-new/utils/features.ts
@@ -6,17 +6,20 @@ export const APP_VERSION_FLAG_FEATURE = {
   value: getAppEnv(),
 };
 
-export const SWITCH_NETWORK_FEATURE = {
-  describe: 'switch network button',
+export const SWITCH_NETWORK_FEATURE_FULL_FEATURE = {
+  describe: 'switch network list',
   allow: isDev,
 };
 
+export const SWITCH_TEST_NETWORK_FEATURE = {
+  describe: 'switch test network list',
+  allow: isQA || isDev,
+};
 
 export const ACTIVITY_DB_STATUS_FEATURE = {
   describe: 'show activity db status after action',
   allow: isQA || isDev,
 };
-
 
 export const ENABLE_WALLET_CONNECT_FEATURE = {
   describe: 'wallet connect feature',
