47ce8660cae23fa32250ca6cb744fb3c65a64ed1
iosh <454053152@qq.com>
2024-05-23 16:00:20 +0800

    chore: update i18n

--- packages/ui-new/pages/WalletConnect/loading.tsx
+++ packages/ui-new/pages/WalletConnect/loading.tsx
@@ -1,14 +1,16 @@
 import BottomSheet, { snapPoints, type BottomSheetMethods } from '@components/BottomSheet';
+import Spinner from '@components/Spinner';
 import Plugins from '@core/WalletCore/Plugins';
 import { WalletConnectPluginEventMethod } from '@core/WalletCore/Plugins/WalletConnect/types';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useTheme } from '@react-navigation/native';
 import { StackNavigation } from '@router/configs';
 import { useEffect } from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { filter } from 'rxjs';
 
 function WalletConnectLoading() {
   const navigation = useNavigation<StackNavigation>();
+  const { colors, reverseColors, mode } = useTheme();
   useEffect(() => {
     const sub = Plugins.WalletConnect.events.pipe(filter((event) => event.type === WalletConnectPluginEventMethod.LOADING)).subscribe((event) => {
       if (event.data === false) {
@@ -23,11 +25,18 @@ function WalletConnectLoading() {
 
   return (
     <BottomSheet enablePanDownToClose={false} isRoute snapPoints={snapPoints.percent75}>
-      <View>
-        <Text>Loading</Text>
+      <View style={styles.loading}>
+        <Spinner width={50} height={50} color={reverseColors[mode === 'light' ? 'iconPrimary' : 'textSecondary']} backgroundColor={colors.iconPrimary} />
       </View>
     </BottomSheet>
   );
 }
 
+const styles = StyleSheet.create({
+  loading: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 export default WalletConnectLoading;
