def4f66d57e23c07cafd17a157d71f5a7b3839e4
mosi <mosshengqi@outlook.com>
2024-05-28 16:30:22 +0800

    fix: remove code

--- packages/core/WalletCore/Methods/signatureMethod.ts
+++ packages/core/WalletCore/Methods/signatureMethod.ts
@@ -7,23 +7,11 @@ import { Signature } from '@core/database/models/Signature';
 @injectable()
 export class SignatureMethod {
   @inject(Plugins) plugins!: Plugins;
-  async createSignature(params: SignatureSubjectValue, prepareCreate?: true): Promise<Signature> {
+  async createSignature(params: SignatureSubjectValue): Promise<Signature> {
     try {
       const { address, app, message, signType } = params;
       const network = await address.network;
       const blockNumber = await this.plugins.BlockNumberTracker.getNetworkBlockNumber(network);
-      for (let i = 0; i < 10; i++) {
-        await _createSignature(
-          {
-            address,
-            app,
-            signType,
-            blockNumber,
-            message: `${message}-${i}`,
-          },
-          prepareCreate,
-        );
-      }
       // TODO: fill params
       return _createSignature(
         {
@@ -33,7 +21,6 @@ export class SignatureMethod {
           blockNumber,
           message,
         },
-        prepareCreate,
       );
     } catch (error) {
       console.error('createSignature error: ', error);
