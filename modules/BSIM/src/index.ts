c7cc73d0a1cb51835ff341fe1884121138e1702c
iosh <454053152@qq.com>
2024-06-07 16:41:25 +0800

    feat: update bsim

--- modules/BSIM/src/index.ts
+++ modules/BSIM/src/index.ts
@@ -1,19 +1,21 @@
-export {CoinTypes} from './types';
+export { CoinTypes } from './types';
+export { BSIMError } from './errors';
 
-export {genNewKey, type GenNewKeyErrorType} from './genNewKey';
+export { genNewKey, type GenNewKeyErrorType } from './genNewKey';
 
-export {getVersion} from './getVersion';
+export { getVersion } from './getVersion';
 
-export {getBSIMVersion, type GetBSIMVersionErrorType} from './getBSIMVersion';
-
-export {getPublicKeyAndAddress} from './getPublicKeyAndAddress';
-
-export {verifyBPIN, type VerifyBPINErrorType} from './verifyBPIN';
-
-export {updateBPIN, type UpdateBPINErrorType} from './updateBPIN';
+export { getBSIMVersion, type GetBSIMVersionErrorType } from './getBSIMVersion';
 
 export {
-  signMessage,
-  type SignMessageReturnType,
-  type SignMessageErrorType,
-} from './signMessage';
+  getPublicKeyAndAddress,
+  type GetPublicKeyAndAddressReturnType,
+  type PublicKeyAndAddress503Type,
+  type PublicKeyAndAddress60Type,
+} from './getPublicKeyAndAddress';
+
+export { verifyBPIN, type VerifyBPINErrorType } from './verifyBPIN';
+
+export { updateBPIN, type UpdateBPINErrorType } from './updateBPIN';
+
+export { signMessage, type SignMessageReturnType, type SignMessageErrorType } from './signMessage';
