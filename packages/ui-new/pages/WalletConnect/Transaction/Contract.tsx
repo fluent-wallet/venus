7a772a0f8eef927584f378c712d0e1da48ad6b6f
iosh <454053152@qq.com>
2024-06-25 16:28:45 +0800

    chore: add symbol to approve

--- packages/ui-new/pages/WalletConnect/Transaction/Contract.tsx
+++ packages/ui-new/pages/WalletConnect/Transaction/Contract.tsx
@@ -67,6 +67,7 @@ function Contract({
               <Text style={[styles.font22, styles.value, { color: parseData.isUnlimited ? colors.textNotice : colors.textPrimary }]} numberOfLines={1}>
                 {customAllowance ? customAllowance.toString() : parseData.isUnlimited ? t('wc.dapp.tx.unlimited') : getFormatValue(parseData.value)}
               </Text>
+              {parseData.symbol && <Text style={styles.font22}>{parseData.symbol}</Text>}
               {parseData && isApproveMethod(parseData) && parseData.assetType === AssetType.ERC20 ? <ModifyIcon width={24} height={24} /> : null}
             </Pressable>
           </View>
