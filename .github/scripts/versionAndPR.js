807b7b51dbc9429025ee4e6defb3cff2f5abbf94
iosh <454053152@qq.com>
2024-08-29 14:51:22 +0800

    chore: up script

--- .github/scripts/versionAndPR.js
+++ .github/scripts/versionAndPR.js
@@ -1,6 +1,33 @@
-module.exports = async ({ github, context, core, exec }) => {
+const fs = require("fs/promises");
+const path = require("path");
 
-  let versionBranch = `qa-release/dev`;
+async function updateVersion(pkgVersion) {
+  // android
+  const androidFilePath = path.join(__dirname, "../../android/app/build.gradle");
+  const data = await fs.readFile(androidFilePath, "utf8");
+  const versionCodeMatch = data.match(/versionCode (\d+)/);
+
+  const currentVersionCode = parseInt(versionCodeMatch[1], 10);
+
+  const updatedData = data.replace(/versionCode (\d+)/, `versionCode ${currentVersionCode + 1}`)
+  .replace(/versionName "(\d+\.\d+\.\d+)"/, `versionName "${pkgVersion}"`);
+
+  await fs.writeFile(androidFilePath, updatedData);
+
+  // ios
+  const iosFilePath = path.join(__dirname, "../../ios/BIMWallet.xcodeproj/project.pbxproj");
+  const iosData = await fs.readFile(iosFilePath, "utf8");
+
+  const currentProjectVersionMatch = iosData.match(/CURRENT_PROJECT_VERSION = (\d+);/);
+  const currentProjectVersion = parseInt(currentProjectVersionMatch[1], 10);
+
+  const updatedIosData = iosData.replace(/CURRENT_PROJECT_VERSION = (\d+);/g, `CURRENT_PROJECT_VERSION = ${currentProjectVersion + 1};`);
+
+  await fs.writeFile(iosFilePath, updatedIosData);
+}
+
+module.exports = async ({ github, context, core, exec }) => {
+  const versionBranch = `qa-release/dev`;
 
   // switch to branch
 
@@ -13,8 +40,23 @@ module.exports = async ({ github, context, core, exec }) => {
   if (isCreatingBranch) {
     await exec.exec("git", ["checkout", "-b", versionBranch]);
   }
+  // reset the qa-release/dev any change 
+  await exec.exec("git", ["reset", `--hard`, context.sha]);
 
+  // yarn version apply
+  await exec.exec("yarn", ["version", "apply"])
+  
+  // remove the version 
+  await exec.exec("rm", ["-rf", path.join(path.resolve(), "../../.yarn/versions")])
 
+  // get current package.json version
+  const pkg = require(path.join(path.resolve(), "../../package.json"));
+  
+  // update android and ios version
+
+  await updateVersion(pkg.version)
+
+  // git add and commit
   const {stdout} = await exec.getExecOutput("git", ["status", "--porcelain"])
 
   if (stdout.length > 0) {
@@ -29,13 +71,15 @@ module.exports = async ({ github, context, core, exec }) => {
   await exec.exec("git", ["push", "--force", "origin", `HEAD:${versionBranch}`]);
 
 
-
   // exist pull request
 
   const finalPrTitle = `Release QA version`;
   const prBody = `This PR is created by github action.
+
   It update the Android versionCode (increment 1) and versionName(read from package.json).
+
   And it also update the IOS CURRENT_PROJECT_VERSION (increment 1). but is not update the IOS MARKETING_VERSION
+  
   `;
 
   let { data: pullRequests } = await github.rest.pulls.list({
