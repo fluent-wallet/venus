09221f5d778222f5670db42b290714084d69d3b9
iosh <ioshx@foxmail.com>
2024-12-09 14:01:50 +0800

    chore: fix get next version

--- .github/scripts/getQaReleaseVersion.js
+++ .github/scripts/getQaReleaseVersion.js
@@ -2,24 +2,24 @@ module.exports = async ({github, context, PACKAGE_VERSION}) => {
     const {
       repo: {owner, repo},
     } = context
-    const nextver = PACKAGE_VERSION
+    const version = PACKAGE_VERSION
     let tags = await github.rest.repos.listTags({owner, repo})
     tags = (tags && tags.data) || []
-    const re = new RegExp(`v${nextver}-rc\.`)
+    const re = new RegExp(`v${version}-qa\.`)
   
-    const nextrc =
-      tags.reduce((nextrc, {name}) => {
-        if (!re.test(name)) return nextrc
-        const rc = parseInt(name.replace(re, ''), 10)
-        if (!rc) return nextrc
-        return Math.max(nextrc, rc)
+    const next_sub_version =
+      tags.reduce((version, {name}) => {
+        if (!re.test(name)) return version
+        const sub_version = parseInt(name.replace(re, ''), 10)
+        if (!sub_version) return version
+        return Math.max(version, sub_version)
       }, 0) + 1
   
     return {
-      version: `${nextver}-qa.${nextrc}`,
-      tag: `v${nextver}-qa.${nextrc}`,
-      prod_version: `${nextver}`,
-      prod_tag: `v${nextver}`,
+      version: `${version}-qa.${next_sub_version}`,
+      tag: `v${version}-qa.${next_sub_version}`,
+      prod_version: `${version}`,
+      prod_tag: `v${version}`,
     }
   }
   
