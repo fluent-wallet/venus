a420b6614e38e1f0da67384d400dc1ad1adc7cf0
iosh <ioshx@foxmail.com>
2024-12-10 14:49:06 +0800

    chore: update github actions add app version to env

--- packages/ui-new/utils/getEnv.ts
+++ packages/ui-new/utils/getEnv.ts
@@ -6,3 +6,5 @@ export const isQA = Config.APP_ENV === 'qa';
 export const isProd = Config.APP_ENV === 'prod';
 
 export const getAppEnv = () => Config.APP_ENV;
+
+export const getAPPVersion = () => isDev ? "dev": Config.APP_VERSION
