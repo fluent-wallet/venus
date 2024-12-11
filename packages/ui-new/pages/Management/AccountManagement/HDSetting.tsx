626175f5a2596de27c8ccdc19dd377f638ffda61
SSSensational <sss18201672034@163.com>
2024-12-11 15:25:10 +0800

    fix: BSIM Account HDSetting

--- packages/ui-new/pages/Management/AccountManagement/HDSetting.tsx
+++ packages/ui-new/pages/Management/AccountManagement/HDSetting.tsx
@@ -139,7 +139,9 @@ const HDManagement: React.FC<StackScreenProps<typeof HDSettingStackName>> = ({ n
       const oldAccountsNeedShow = await Promise.all(_oldAccountsNeedShow.map((account) => queryAccountById(account.id)));
 
       const batchNews = await Promise.all(
-        newAccountsInChoose.map((account) => methods.addAccount({ accountGroup, index: account.index, vaultData: mnemonic }, true)),
+        newAccountsInChoose.map((account) =>
+          methods.addAccount({ accountGroup, index: account.index, vaultData: mnemonic, hexAddress: account.addressValue }, true),
+        ),
       );
 
       await database.write(async () => {
