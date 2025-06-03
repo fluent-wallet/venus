9b34b786bfacf18c583fbb38c06574b118470a2e
iosh <ioshx@foxmail.com>
2025-06-03 18:17:12 +0800

    chore: fix ios

--- ios/BIMWallet/AppDelegate.swift
+++ ios/BIMWallet/AppDelegate.swift
@@ -26,7 +26,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "BIMWallet",
+      withModuleName: "BIM Wallet",
       in: window,
       launchOptions: launchOptions
     )
