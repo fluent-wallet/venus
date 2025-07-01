b7ecefc4b5a018f524baa79a9eb9c31bc9d33823
iosh <ioshx@foxmail.com>
2025-07-01 18:10:29 +0800

    add type

--- packages/core/WalletCore/Signer/types.ts
+++ packages/core/WalletCore/Signer/types.ts
@@ -1,9 +1,20 @@
+import type { Vault } from '@core/database/models/Vault';
+import type { IKeyring } from '../Keyring/types';
+
 export enum ChainFamily {
   EVM = 'evm',
   CFX = 'cfx',
 }
 
-export interface Signer {
+export interface SoftwareSignerFactoryParams {
+  vault: Vault;
+  password: string;
+  path?: string;
+  index?: number;
+  keyring: IKeyring;
+}
+
+export interface ISigner {
   getAddress(chain: ChainFamily): Promise<string>;
 
   // TODO update this type
@@ -12,3 +23,5 @@ export interface Signer {
   // TODO update this type
   signTransaction(tx: any, chain: ChainFamily): Promise<any>;
 }
+
+export type SoftwareSignerFactory = (params: SoftwareSignerFactoryParams) => Promise<ISigner>;
